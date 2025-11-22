2312e5b382ff4c965718106a130769f65d5d3634
Jimena Hernández <jihernandezc@unal.edu.co>
2025-11-22 12:04:32 -0500

    eliminar constantes

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,3 +1,5 @@
+# obtain a list of files in the input directory
+
 from ._internals.count_words import count_words
 from ._internals.preprocess_lines import preprocess_lines
 from ._internals.read_all_lines import read_all_lines
@@ -6,11 +8,14 @@ from ._internals.write_count_words import write_count_words
 
 
 def main():
-    all_lines = read_all_lines()
+    input_folder = "data/input/"
+    output_folder = "data/output/"
+
+    all_lines = read_all_lines(input_folder)
     all_lines = preprocess_lines(all_lines)
     words = split_into_words(all_lines)
     counter = count_words(words)
-    write_count_words(counter)
+    write_count_words(counter, output_folder)
 
 
 if __name__ == "__main__":
